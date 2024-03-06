9f8522c5c9df1ed658eb1923ba1c46b220507c44
Anatolii <toliankir@gmail.com>
2024-03-06 08:53:14 +0100

    First implementation

---
+++ README.md
@@ -0,0 +1,13 @@
+## Client
+
+### Get started
+
+#### Required
+
+Browser-sync - https://browsersync.io/
+
+#### Start app
+
+```shell
+browser-sync start --config config.js 
+```
---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "ui": {
+        "port": 3001,
+        "weinre": {
+            "port": 8080
+        }
+    },
+    "files": ".",
+    server: {
+        baseDir: ".",
+    },
+    "watchEvents": [
+        "change"
+    ],
+    reloadOnRestart: true,
+    "port": 3000
+};
